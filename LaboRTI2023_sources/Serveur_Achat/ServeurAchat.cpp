28b0554ec80087bfa7c1f9edb6c2d9a8befe740c
maximerenardhepl <maximerenardhepl@gmail.com>
2023-09-24 12:23:43 +0200

    commencement du serveur en pool

--- LaboRTI2023_sources/Serveur_Achat/ServeurAchat.cpp
+++ LaboRTI2023_sources/Serveur_Achat/ServeurAchat.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <pthread.h>
+#include <signal.h>
 #include "LibSocket.h"
 using namespace std;
 
+void HandlerSIGINT(int s);
+void* FctThreadClient(void* p);
+
+#define POOL_MAX 10
+int TabSocket[20] = {0};
+pthread_mutex_t mutexSocketsAcceptees;
+pthread_cond_t condSocketsAcceptees;
+int indiceEcriture=0, indiceLecture=0;
+    
 //nombre para/ port
 int main(int argc, char* argv[])
-{
+{   
+    
     //vérifie la présence d'argument
     if (argc != 2)
     {
@@ -12,6 +24,21 @@ int main(int argc, char* argv[])
         printf("USAGE : ServeurAchat portServeur\n");
         exit(1);
     }
+
+    // Armement des signaux
+    struct sigaction A;
+    A.sa_flags = 0;
+    sigemptyset(&A.sa_mask);
+    A.sa_handler = HandlerSIGINT;
+
+    if (sigaction(SIGINT,&A,NULL) == -1)
+    {
+        perror("Erreur de sigaction");
+        exit(1);
+    }
+    
+    //initialisation
+    pthread_t th;
 
     int sEcoute = 0;
     //je passe un tableau d'argument avec donc l'ip et le port
@@ -21,15 +48,46 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
+    //création du pool de thread
+    for(int i=0 ; i<POOL_MAX ; i++)
+    {
+        pthread_create(&th,NULL,FctThreadClient,NULL);
+    }
+
+
+    //lancement de la boucle du serveur
     int sService = 0;
     while(1)
     {
         char ipClient[50];
         if((sService = Accept(sEcoute, ipClient)) != -1)
         {
-            
+            pthread_mutex_lock(&mutexSocketsAcceptees);
+            TabSocket[indiceEcriture] = sService;
+            indiceEcriture++;
+            pthread_mutex_unlock(&mutexSocketsAcceptees);
+            pthread_cond_signal(&condSocketsAcceptees);
         }
     }
+    return 0;
+}
+
+void* FctThreadClient(void* p)
+{
+    int Ssocket=0;
+
+    pthread_mutex_lock(&mutexSocketsAcceptees);
+    //ce réveille
+    pthread_cond_wait(&condSocketsAcceptees,&mutexSocketsAcceptees);
+
+    Ssocket = TabSocket[indiceLecture];
+
+    pthread_mutex_unlock(&mutexSocketsAcceptees);
 
     return 0;
+}
+
+void HandlerSIGINT(int s)
+{
+
 }
