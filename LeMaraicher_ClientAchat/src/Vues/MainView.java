72e74bd88b26938db73954caaaa8cac33bc6e149
NoaLallemand <noa.lallemand@student.hepl.be>
2023-10-23 20:13:32 +0200

    Debut Client Achat Java : Conception GUI

---
+++ LeMaraicher_ClientAchat/src/Vues/MainView.java
@@ -0,0 +1,19 @@
+package Vues;
+
+import javax.swing.*;
+
+public class MainView {
+    private JButton btnPrecedent;
+    private JButton btnSuivant;
+    private JButton btnAcheter;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JSpinner spinner1;
+    private JTextPane textPane1;
+    private JTable table1;
+    private JButton supprimerArticleButton;
+    private JButton viderLePanierButton;
+    private JButton confirmerAchatButton;
+    private JTextField textField4;
+}
