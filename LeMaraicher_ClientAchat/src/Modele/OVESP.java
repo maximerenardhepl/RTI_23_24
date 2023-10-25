837eb220315960a4c69069983e78c313ddd86dd0
NoaLallemand <noa.lallemand@student.hepl.be>
2023-10-25 10:16:50 +0200

    Creation Classe OVESP

---
+++ LeMaraicher_ClientAchat/src/Modele/OVESP.java
@@ -0,0 +1,27 @@
+package Modele;
+
+public class OVESP {
+    private char[] requete;
+    private char[] reponse;
+
+    private static OVESP ovesp = new OVESP();
+
+    private OVESP() {}
+    public OVESP getOvesp() { return ovesp; }
+
+    public void login() {
+
+    }
+
+    public void logout() {
+
+    }
+
+    public void cancel() {
+
+    }
+
+    public void cancelAll() {
+
+    }
+}
